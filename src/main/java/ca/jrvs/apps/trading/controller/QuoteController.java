d5a110045997712203537dbb3da4d6875d40813d
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-08-05 19:58:44 -0400

    Initial commit

---
+++ src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -0,0 +1,101 @@
+package ca.jrvs.apps.trading.controller;
+
+import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
+import ca.jrvs.apps.trading.model.dto.IexQuote;
+import ca.jrvs.apps.trading.model.dto.Quote;
+import ca.jrvs.apps.trading.service.QuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/quote")
+public class QuoteController {
+
+    private QuoteService quoteService;
+    private QuoteDao quoteDao;
+    private MarketDataDao marketDataDao;
+
+    @Autowired
+    public QuoteController(QuoteService quoteService, QuoteDao quoteDao,
+                           MarketDataDao marketDataDao) {
+        this.quoteService = quoteService;
+        this.quoteDao = quoteDao;
+        this.marketDataDao = marketDataDao;
+    }
+
+    @PutMapping(path = "/iexMarketData")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateMarketData() {
+        try {
+            quoteService.updateMarketData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    public void putQuote(@RequestBody Quote quote) {
+        try {
+            quoteDao.update(Collections.singletonList(quote));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping(path = "/tickerId/{tickerId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createQuote(@PathVariable String tickerId) {
+        try {
+            quoteService.initQuote(tickerId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @GetMapping(path = "/dailyList")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<Quote> getDailyList() {
+        try {
+            return quoteDao.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(path = "/iex/ticker/{ticker}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public IexQuote getQuote(@PathVariable String ticker) {
+        try {
+            return marketDataDao.UnmarshallJson(ticker);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(path = "/iex/tickersInBatch/{ticker}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<IexQuote> getBatchQuote(@PathVariable String ticker) {
+        try {
+            List<String> tickers = new ArrayList<String>(Arrays.asList(ticker.split(",")));
+
+            return marketDataDao.UnmarshallJson(tickers);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/ca/jrvs/apps/trading/controller/TraderController.java
@@ -0,0 +1,96 @@
+package ca.jrvs.apps.trading.controller;
+
+import ca.jrvs.apps.trading.dao.TraderDao;
+import ca.jrvs.apps.trading.model.dto.Account;
+import ca.jrvs.apps.trading.model.dto.Trader;
+import ca.jrvs.apps.trading.model.view.TraderAccountView;
+import ca.jrvs.apps.trading.service.FundTransferService;
+import ca.jrvs.apps.trading.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("/trader")
+public class TraderController {
+
+    private RegisterService registerService;
+    private TraderDao traderDao;
+    private FundTransferService fundTransferService;
+
+    @Autowired
+    public TraderController(RegisterService registerService, TraderDao traderDao,
+                            FundTransferService fundTransferService) {
+        this.registerService = registerService;
+        this.traderDao = traderDao;
+        this.fundTransferService = fundTransferService;
+    }
+
+    @DeleteMapping(path = "/traderId/{traderId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteTrader(@PathVariable Integer traderId) {
+        try {
+            registerService.deleteTraderById(traderId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public TraderAccountView putTraderandAccount(@RequestBody Trader trader) {
+        try {
+            return registerService.createTraderAndAccount(trader);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping(path = "/trader/firstname/{firstname}/lastname/{lastname}/dob/{dob}/country/{country}/email/{email}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public TraderAccountView createTraderandAccount(@PathVariable String firstname,
+                                                    @PathVariable String lastname,
+                                                    @PathVariable String dob,
+                                                    @PathVariable String country,
+                                                    @PathVariable String email) {
+        try {
+            Trader trader = new Trader();
+            trader.setFirstName(firstname);
+            trader.setLastName(lastname);
+            trader.setDob(LocalDate.parse(dob));
+            trader.setCountry(country);
+            trader.setEmail(email);
+            return registerService.createTraderAndAccount(trader);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping(path = "/deposit/traderId/{traderId}/amount/{amount}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Account deposit(@PathVariable Integer traderId, @PathVariable Double amount) {
+        try {
+            return fundTransferService.deposit(traderId, amount);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping(path = "/withdraw/traderId/{traderId}/amount/{amount}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Account withdraw(@PathVariable Integer traderId, @PathVariable Double amount) {
+        try {
+            return fundTransferService.withdraw(traderId, amount);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
