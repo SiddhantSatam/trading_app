8484fcbd6244d460a62ec64386d4d9a497f31ce7
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-08-05 20:01:12 -0400

    Initial commit

---
+++ src/main/java/ca/jrvs/apps/trading/util/JsonUtil.java
@@ -0,0 +1,38 @@
+package ca.jrvs.apps.trading.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+
+public class JsonUtil {
+
+    //Converting to Json from Java object
+    public static String toJson(Object object) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(object);
+    }
+
+    /**
+     * Parse JSON string to a object
+     *
+     * @param json  JSON str
+     * @param clazz object class
+     * @param <T>   Type
+     * @return Object
+     * @throws IOException
+     */
+    public static <T> T toObjectFromJson(String json, Class clazz)  throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return (T) mapper.readValue(json, clazz);
+    }
+
+}
