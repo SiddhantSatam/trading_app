1e36e70b5cd8e6cd6483e7dfce6129be50c7c4c5
SiddhantSatam <51959242+SiddhantSatam@users.noreply.github.com>
2019-08-10 15:15:39 -0400

    Final update

--- src/main/java/ca/jrvs/apps/trading/service/OrderService.java
+++ src/main/java/ca/jrvs/apps/trading/service/OrderService.java
@@ -1,4 +1,92 @@
 package ca.jrvs.apps.trading.service;
 
+import ca.jrvs.apps.trading.dao.AccountDao;
+import ca.jrvs.apps.trading.dao.PositionDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
+import ca.jrvs.apps.trading.dao.SecurityOrderDao;
+import ca.jrvs.apps.trading.model.dto.MarketOrderDto;
+import ca.jrvs.apps.trading.model.dto.SecurityOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
 public class OrderService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+    private AccountDao accountDao;
+    private SecurityOrderDao securityOrderDao;
+    private QuoteDao quoteDao;
+    private PositionDao positionDao;
+
+    @Autowired
+    public OrderService(AccountDao accountDao, SecurityOrderDao securityOrderDao, QuoteDao quoteDao, PositionDao positionDao) {
+        this.accountDao = accountDao;
+        this.securityOrderDao = securityOrderDao;
+        this.quoteDao = quoteDao;
+        this.positionDao = positionDao;
+    }
+    public SecurityOrder executeMarketOrder(List<MarketOrderDto> orderDtos) {
+        Double askPrice = 0.0;
+        Double amount = 0.0;
+        SecurityOrder securityOrder = new SecurityOrder();
+        for (MarketOrderDto orderDto : orderDtos) {
+            if (orderDto.getAccountId() < 0 || orderDto.getTicker() == null) {
+                throw new IllegalArgumentException("Invalid input");
+            }
+
+            securityOrder.setAccountId(orderDto.getAccountId());
+            securityOrder.setSize(orderDto.getSize());
+            securityOrder.setTicker(orderDto.getTicker());
+            try {
+                askPrice = (quoteDao.findById(orderDto.getTicker())).getAskPrice();
+                logger.info(String.valueOf(askPrice));
+                amount = (accountDao.findById(orderDto.getAccountId())).getAmount();
+                logger.info(String.valueOf(amount));
+            } catch (DataAccessException e) {
+                e.getMessage();
+            }
+            if (orderDto.getSize() > 0)
+                buy(amount, askPrice, securityOrder, orderDto);
+
+            else
+                sell(amount, askPrice, securityOrder, orderDto);
+
+        }
+        SecurityOrder securityOrder1 = securityOrderDao.save(securityOrder);
+        return securityOrder1;
+    }
+
+    protected void buy(Double amount, Double askPrice, SecurityOrder securityOrder, MarketOrderDto orderDto) {
+        Double securityCost = (orderDto.getSize() * askPrice);
+        if (amount < securityCost) {
+            securityOrder.setStatus(SecurityOrder.orderStatus.CANCELLED);
+            securityOrder.setNotes("You have less money in account, minimum should be " + securityCost);
+        } else {
+            securityOrder.setPrice(askPrice);
+            securityOrder.setStatus(SecurityOrder.orderStatus.FILLED);
+            securityOrder.setNotes(null);
+            accountDao.updateAccountbyID(amount - securityCost, orderDto.getAccountId());
+        }
+    }
+
+    protected void sell(Double amount, Double askPrice, SecurityOrder securityOrder, MarketOrderDto orderDto) {
+        Long pos = positionDao.getPosition(orderDto.getAccountId(), orderDto.getTicker());
+
+        if (pos > -(orderDto.getSize())) {
+            Double securityCost = -(orderDto.getSize() * askPrice);
+            securityOrder.setPrice(askPrice);
+            securityOrder.setStatus(SecurityOrder.orderStatus.FILLED);
+            securityOrder.setNotes(null);
+            accountDao.updateAccountbyID(amount + securityCost, orderDto.getAccountId());
+        } else {
+            securityOrder.setStatus(SecurityOrder.orderStatus.CANCELLED);
+            securityOrder.setNotes("You have less security in account");
+        }
+    }
 }
